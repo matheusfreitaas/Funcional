ad69cad7259d75b657465a155affc4b0d7226a25
Matheus Freitas <matheusbfreitas@gmail.com>
2019-03-20 16:44:09 -0300

    Adiciona lista finalizada

--- lista-funcoes/lista.hs
+++ lista-funcoes/lista.hs
@@ -3,5 +3,21 @@ divTuple (1, 1) = 1
 divTuple (x, 1) = x/1
 divTuple (x, y) = x/y
 
-
 somatorio a b = sum [a..b]
+
+somatorioRecursivo a b = if (a + 1 == b) then a + b else a + somatorioRecursivo (a+1) b
+
+square x = x*x
+
+sumSquares x y = square x + square y
+
+highOrderSum f a b = sum (map f [a,b])
+
+hoSumSquares a b = highOrderSum square a b
+
+isEven a = if mod a 2 == 0 then True else False
+
+mapFilter f p xs = filter isEven (map f xs)
+
+
+
